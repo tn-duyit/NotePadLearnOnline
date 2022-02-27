2c456d3c04d3da9fb4db845377b96b4bbf10c0b4
tn-duyit <tn.duy2024@gmail.com>
2022-02-27 17:29:16 +0700

    codecuaduy

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,8 @@
+import {Redirect} from 'react-router-dom'
+
+const Landing = () => {
+    return( <Redirect to = '/login'/>
+       
+    )
+}
+export default Landing
---
+++ client/src/components/posts/AddPostModal.js
@@ -0,0 +1,99 @@
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { useContext, useState } from 'react'
+import { PostContext } from '../../contexts/PostContext'
+
+const AddPostModal = () => {
+	// Contexts
+	const {
+		showAddPostModal,
+		setShowAddPostModal,
+		addPost,
+		setShowToast
+	} = useContext(PostContext)
+
+	// State
+	const [newPost, setNewPost] = useState({
+		title: '',
+		description: '',
+		url: '',
+		status: 'HỌC'
+	})
+
+	const { title, description, url } = newPost
+
+	const onChangeNewPostForm = event =>
+		setNewPost({ ...newPost, [event.target.name]: event.target.value })
+
+	const closeDialog = () => {
+		resetAddPostData()
+	}
+
+	const onSubmit = async event => {
+		event.preventDefault()
+		const { success, message } = await addPost(newPost)
+		resetAddPostData()
+		setShowToast({ show: true, message, type: success ? 'success' : 'danger' })
+	}
+
+	const resetAddPostData = () => {
+		setNewPost({ title: '', description: '', url: '', status: 'HỌC' })
+		setShowAddPostModal(false)
+	}
+
+	return (
+		<Modal show={showAddPostModal} onHide={closeDialog}>
+			<Modal.Header closeButton>
+				<Modal.Title>Bạn cần học gì?</Modal.Title>
+			</Modal.Header>
+			<Form onSubmit={onSubmit}>
+				<Modal.Body>
+					<Form.Group>
+						<Form.Control
+							type='text'
+							placeholder='Nội dung'
+							name='title'
+							required
+							aria-describedby='title-help'
+							value={title}
+							onChange={onChangeNewPostForm}
+						/>
+						<Form.Text id='title-help' muted>
+							Yêu cầu
+						</Form.Text>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							as='textarea'
+							rows={3}
+							placeholder='Mô tả'
+							name='description'
+							value={description}
+							onChange={onChangeNewPostForm}
+						/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							type='text'
+							placeholder='Đường dẫn mô tả'
+							name='url'
+							value={url}
+							onChange={onChangeNewPostForm}
+						/>
+					</Form.Group>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button variant='secondary' onClick={closeDialog}>
+						Hủy
+					</Button>
+					<Button variant='primary' type='submit'>
+						Tạo
+					</Button>
+				</Modal.Footer>
+			</Form>
+		</Modal>
+	)
+}
+
+export default AddPostModal
